7feb4dca708092b931dfad5a8236ef22b6e3d778
Narendran S P <120792289+narendransp@users.noreply.github.com>
2025-08-23 16:55:42 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://recipemanager-4g1t.onrender.com'});
+const API = axios.create({ baseURL: 'https://recipemanager-4g1t.onrender.com/api'});
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
